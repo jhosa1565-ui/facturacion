4794be3a2a6b7ccc23946e2a0c6934d7186afb0d
EXPERTO ITSI <experto@ITSI.EDU>
2026-08-28 16:56:44 -0500

    [ADD] Creacion de template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,15 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
-    }
-
-    public function saludo($nombre,$apellido){
-        echo "Hola " . $nombre . " " . $apellido;
-    }
-
-    public function sumita($n1, $n2){
-              $suma = $n1 + $n2;
-              echo "El resultado es: $suma";
+        return view('facturacion/index');
     }
 }
